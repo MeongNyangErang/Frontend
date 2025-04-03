c06c0c4d5f9043db8c80f2e143181fe36f96c6b4
MinJuPark52 <als4013@naver.com>
2025-04-03 17:55:27 +0900

    fix: move accommodation style

--- src/pages/MyPage/host/register/styles.tsx
+++ src/pages/MyPage/host/register/styles.tsx
@@ -118,3 +118,121 @@ export const SFormContainer = styled.div`
   gap: 5px;
   margin-bottom: 10px;
 `;
+
+/* 숙소 Style */
+
+export const SSFieldset = styled.fieldset`
+  font-family: 'Noto Sans KR';
+  padding: 16px;
+  border: none;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SSOptionSelectorWrapper = styled.div`
+  margin-bottom: 8px;
+`;
+
+export const SSLabel = styled.label`
+  font-family: 'Noto Sans KR';
+  display: block;
+  font-size: 1rem;
+  margin: 10px 0;
+`;
+
+export const SSLabelFile = styled.label`
+  font-family: 'Noto Sans KR';
+  display: block;
+  font-size: 1rem;
+  margin-top: 20px;
+  padding-bottom: 10px;
+`;
+
+export const SSInput = styled.input`
+  font-family: 'Noto Sans KR';
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  border: 1px solid #d1d5db;
+  border-radius: 4px;
+  font-size: 1rem;
+`;
+
+export const SSDescriptionWrapper = styled.div`
+  position: relative;
+`;
+
+export const SSInputExplain = styled.textarea`
+  font-family: 'Noto Sans KR';
+  width: 100%;
+  height: 100px;
+  padding: 8px;
+  border: 1px solid #d1d5db;
+  border-radius: 4px;
+  font-size: 1rem;
+  resize: none;
+  overflow-y: auto;
+`;
+
+export const SSInputAddress = styled.input`
+  font-family: 'Noto Sans KR';
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 7px;
+  border: 1px solid #d1d5db;
+  border-radius: 4px;
+  font-size: 1rem;
+`;
+
+export const SSCharacterCount = styled.div`
+  position: absolute;
+  bottom: 8px;
+  right: 8px;
+  font-size: 0.875rem;
+  color: #6b7280;
+`;
+
+export const SSButton = styled.button`
+  background-color: var(--sub-color);
+  color: white;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 10px;
+  margin-left: auto;
+  &:hover {
+    background-color: var(--sub-color);
+    border: 1px solid #f03e5e;
+  }
+`;
+
+export const SSImagePreviewWrapper = styled.div`
+  margin-top: 10px;
+  width: 50%;
+  height: 150px;
+  border: 2px dashed #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: contain;
+  }
+`;
+
+export const SSPreviewWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 10px;
+  gap: 10px;
+  justify-content: flex-start;
+`;
+
+export const SSInputFile = styled.input`
+  border: 1px solid #ccc;
+  width: 60%;
+  padding: 2px;
+`;
