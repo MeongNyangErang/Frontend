3c138674e212d493798fb26c6886e5bb18ed1976
SehyunKim <allezvvell@gmail.com>
2025-04-18 03:46:22 +0900

    fix: 채팅, 마이페이지 프로필 조회 변경된 api 맞춰 타입, url주소 수정

--- src/components/common/ProfileEdit/NickNameEditForm.tsx
+++ src/components/common/ProfileEdit/NickNameEditForm.tsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { inputStyle, inputVariantStyles } from '@components/styles/mixins';
+
 interface NickNameEditFormProps {
   defaultValue: string;
 }
 
-const NickNameEditForm = ({}: NickNameEditFormProps) => {
-  return <></>;
+const NickNameEditForm = ({ defaultValue }: NickNameEditFormProps) => {
+  const [text, setText] = useState(defaultValue);
+
+  return (
+    <>
+      <SInputWrap>
+        <SInput />
+      </SInputWrap>
+    </>
+  );
 };
 
 export default NickNameEditForm;
+
+const SInputWrap = styled.div`
+  width: 100%;
+`;
+
+const SInput = styled.input`
+  ${inputStyle}
+  ${inputVariantStyles.gray}
+`;
