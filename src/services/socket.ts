69958f2486f00a0f5dc54cee2caad1a0290b377a
SehyunKim <allezvvell@gmail.com>
2025-04-17 14:35:25 +0900

    fix: 채팅 websocket 연결

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,14 +1,29 @@
-import { Client } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
+import { Client } from '@stomp/stompjs';
+import { getLocalStorage } from '@utils/storage';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_SOCKET_BASE_URL;
 
 const socketUrl = `${BASE_URL}/ws`;
 
 export const createStompClient = () => {
+  const token = getLocalStorage('accessToken');
+
   const client = new Client({
     webSocketFactory: () => new SockJS(socketUrl),
     reconnectDelay: 5000,
+    connectHeaders: {
+      Authorization: `Bearer ${token}`,
+    },
+    debug: (str) => {
+      console.log(str);
+    },
+    onStompError: (e) => {
+      console.log(e, 'stomp error');
+    },
+    onWebSocketError: (e) => {
+      console.log(e, 'socket error');
+    },
   });
 
   return client;
