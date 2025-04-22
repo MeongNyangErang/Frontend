d25401750a809aa4648e9f28d1579bbb65bc1444
SehyunKim <allezvvell@gmail.com>
2025-04-23 05:36:45 +0900

    feature: 숙소 찜 버튼 클릭뒤 숙소 검색 query 무효화

--- src/hooks/query/useSearchAccommodations.ts
+++ src/hooks/query/useSearchAccommodations.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { SearchBaseType, SearchFilterType } from '@typings/search';
 import { searchAccommodations } from '@services/search';
 
@@ -7,10 +7,23 @@ export const useSearchAccommodations = (
   page: number,
   filter?: SearchFilterType,
 ) => {
-  return useQuery({
+  const queryClient = useQueryClient();
+  const result = useQuery({
     queryKey: ['search-accommodations', query, page, filter],
     queryFn: () => searchAccommodations(query, page, filter),
     enabled: Object.values(query).every((v) => v !== ''),
-    staleTime: 1000 * 60 * 60,
+    staleTime: 1000 * 60 * 30,
   });
+  const refreshSearchAccommodations = () => {
+    queryClient.invalidateQueries({
+      predicate: (query) => {
+        const queryKey = query.queryKey;
+        return (
+          Array.isArray(queryKey) && queryKey[0] === 'search-accommodations'
+        );
+      },
+    });
+  };
+
+  return { ...result, refreshSearchAccommodations };
 };
