805f16ecf2a2fb285760cd8e8f348b3636f2c1c4
SehyunKim <allezvvell@gmail.com>
2025-04-23 03:33:11 +0900

    fix: 리뷰 수정 디버깅 코드 추가

--- src/hooks/page/useUserReservationListPage.ts
+++ src/hooks/page/useUserReservationListPage.ts
@@ -56,9 +56,7 @@ const useUserReservationListPage = () => {
   const navigate = useNavigate();
 
   const refreshPage = () => {
-    console.log('refreshPage', currentPage, currentTab);
     if (currentPage === 0) return;
-    setReservationList([]);
     setCurrentPage(0);
   };
 
@@ -125,7 +123,6 @@ const useUserReservationListPage = () => {
   useEffect(() => {
     if (!reservationItems) return;
     if (page === 0) {
-      console.log(page, 'reservationItemsPage', currentTab);
       setReservationList([...reservationItems]);
     } else {
       setReservationList((prev) => [...prev, ...reservationItems]);
