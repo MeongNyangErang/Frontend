d10500d1567cbfa5a5de7c9479d5063acc114218
SehyunKim <allezvvell@gmail.com>
2025-04-24 02:39:11 +0900

    fix: 디버깅 코드 제거

--- src/pages/host/HostRegister/RegisterRoom.tsx
+++ src/pages/host/HostRegister/RegisterRoom.tsx
@@ -371,10 +371,6 @@ const RegisterRoom = () => {
       petFacilityTypes,
     } as any;
 
-    console.log(data, 'data');
-
-    return;
-
     if (roomId) data['roomId'] = roomId;
 
     const formData = new FormData();
