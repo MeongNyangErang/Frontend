e0dd3dbc5a89957d7557cef38036068d4f6b409c
SehyunKim <allezvvell@gmail.com>
2025-05-25 16:40:23 +0900

    fix: accessToken 재발급 실패시 사용하는 logoutFn setting위치를 App에서 MainLayout으로 변경

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
 import PortalRoots from '@components/layouts/PortalRoots';
-import useLogout from '@hooks/auth/useLogout';
+
 import AppRouter from './routes';
 
 function App() {
-  useLogout(); // setLogoutFn
-
   return (
     <BrowserRouter>
       <AppRouter />
